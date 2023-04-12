407d5db1c2e470e5514ebabe3b1397fae36bd319
WangJie <WangJie@WangJie.com>
2023-04-12 22:47:24 +0800

    webpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -29,9 +29,9 @@ const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'
 const createEnvironmentHash = require('./webpack/persistentCache/createEnvironmentHash');
 
 // Source maps are resource heavy and can cause out of memory issue for large source files.
-// const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
 // 加快打包速度
-const shouldUseSourceMap = 'false';
+// const shouldUseSourceMap = 'false';
 
 const reactRefreshRuntimeEntry = require.resolve('react-refresh/runtime');
 const reactRefreshWebpackPluginRuntimeEntry = require.resolve(
